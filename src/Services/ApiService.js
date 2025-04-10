1b794c843cf74cfb1c2752ece5d280508391c30c
imnishantmehra <140081121+imnishantmehra@users.noreply.github.com>
2025-04-10 17:05:24 +0530

    Update BE url.

--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-    "https://290b-2405-201-3009-d88a-15f-53be-734c-3b59.ngrok-free.app";
+    "https://339e-2405-201-3009-d88a-bd72-1e2d-aa22-381c.ngrok-free.app";
 
 const axiosConfigForFetch = {
     headers: {
